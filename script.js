d74f3389b6a11ac98679c514c17d5e7d2d7ad666
Anurag Bachheti <157354714+Anurag-Bachheti@users.noreply.github.com>
2025-03-24 12:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function toCase(text) {
-  // write your code here
+	  let lowerCase = text.toLowerCase();
+	  let upperCase = text.toUpperCase();
+	  return lowerCase.concat('-', upperCase)
 }
-
 // DO not change the code below
-
 const text = prompt("Enter text:");
 alert(toCase(text));
